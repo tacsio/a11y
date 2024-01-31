003b5952423e712b39be2ff464fd3ed89cebd807
Tarcisio Coutinho <tcs5cin@gmail.com>
2024-01-31 11:43:58 -0300

    parte 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,38 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { YesNoButtonGroupComponent } from './shared/components/yes-no-button-group/yes-no-button-group.component';
 
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, YesNoButtonGroupComponent],
+  imports: [
+    RouterOutlet,
+    YesNoButtonGroupComponent,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'a11y';
-  yesNoAnswer = 'no';
+  form: FormGroup = null;
+
+  public test = null;
+
+  constructor(formBuilder: FormBuilder) {
+    this.form = formBuilder.group({
+      yesNoAnswer: ['no'],
+    });
+  }
+
+  public submit(): void {
+    console.log(this.form.value);
+  }
 }
--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  forwardRef,
+} from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-yes-no-button-group',
@@ -6,8 +13,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   imports: [],
   templateUrl: './yes-no-button-group.component.html',
   styleUrl: './yes-no-button-group.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef(() => YesNoButtonGroupComponent),
+    },
+  ],
 })
-export class YesNoButtonGroupComponent {
+export class YesNoButtonGroupComponent implements ControlValueAccessor {
   @Input()
   public value: string = null;
 
@@ -19,9 +33,26 @@ export class YesNoButtonGroupComponent {
 
   public options = YesNoButtonGroupOptions;
 
+  public onChange = (value: string) => {};
+  public onTouched = () => {};
+
   activate(value) {
+    this.writeValue(value);
+  }
+
+  writeValue(value: string): void {
     this.value = value;
+    this.onChange(this.value);
     this.valueChange.emit(this.value);
+  }
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+  setDisabledState?(isDisabled: boolean): void {
+    // throw new Error('Method not implemented.');
   }
 }
 
